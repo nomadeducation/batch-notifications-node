c9ace2bdbee02dfd001cf230e3c22ea9598cf9a0
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2017-05-10 15:14:02 +0200

    chore(fixtures): add simple fixtures

---
+++ test/fixtures/campaign.js
@@ -0,0 +1,18 @@
+exports.createMinimal = {
+    payload: {
+        name: "minimal notification",
+        live: false,
+        push_time: "now",
+        messages: [
+            {
+                "language": "fr",
+                "title": "Salut le monde !",
+                "body": "Comment ça va ?"
+            }
+        ]
+    },
+    token: {
+        campaign_token: "3396955c1a7fe0005d76973fca00b44a"
+    },
+    statusCode: 201
+};
