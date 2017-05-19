458f4447c58137232ba750625bcf35aeee31d027
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2017-05-19 11:36:53 +0200

    chore(fixtures): use JSON format in the payload

--- test/fixtures/campaign.js
+++ test/fixtures/campaign.js
@@ -1,9 +1,9 @@
 exports.createMinimal = {
     payload: {
-        name: "minimal notification",
-        live: false,
-        push_time: "now",
-        messages: [
+        "name": "minimal notification",
+        "live": false,
+        "push_time": "now",
+        "messages": [
             {
                 "language": "fr",
                 "title": "Salut le monde !",
@@ -12,7 +12,7 @@ exports.createMinimal = {
         ]
     },
     token: {
-        campaign_token: "3396955c1a7fe0005d76973fca00b44a"
+        "campaign_token": "3396955c1a7fe0005d76973fca00b44a"
     },
     statusCode: 201
 };
