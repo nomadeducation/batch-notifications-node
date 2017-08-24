1b3a7b3e0b337be21485ef7214932057728f07ff
pierrot <pierre@guestviews.co>
2017-08-24 16:12:29 +0200

    chore(fixtures): Add transactional minimum payload

---
+++ test/fixtures/transactional.js
@@ -0,0 +1,16 @@
+exports.postMinimal = {
+    payload: {
+        "group_id": "Welcome_campaign",
+        "recipients": {
+            "custom_ids": ["599ed1d2e8373c8806be063e"]
+        },
+        "message": {
+            "title": "Hello!",
+            "body": "How's it going?"
+        }
+    },
+    token: {
+        "token": "d95b578d-c12e-43e5-a506-ae7423b78320"
+    },
+    statusCode: 201
+};
